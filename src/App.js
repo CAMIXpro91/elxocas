e88d001ef055dd2de832d1103d753af4f1063917
Fabian Pachao <fabianpachao20@gmail.com>
2023-07-20 20:28:00 -0600

    C3 tarea completa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './component/C01componente';
+import C02contador from './component/C02contador';
+import C03dobleestado from './component/C03dobleestado';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Componente 1</h1>
+      <C01componente></C01componente>
+
+      <h2>Componente 2</h2>
+      <C02contador></C02contador>
+
+      <h3>Componente 3</h3>
+      <C03dobleestado></C03dobleestado>
     </div>
   );
 }
