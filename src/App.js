241573e5956c7cd1fb5e6d267af4d81d8c35b124
examen semana 16 - 1 <fabianpachao20@gmail.com>
2023-08-04 08:17:26 -0500

    semana16

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ import C03dobleestado from './component/C03dobleestado';
 import C04variable from './component/C04variable';
 import C05operador from './component/C05operador';
 import C06matriz from './component/C06matriz';
+import C07matrizOperaciones from './component/C07matrizOperaciones';
 
 function App() {
   return (
     <div>
-      <h1>Componente 1:</h1>
-      <C01componente></C01componente>
+      <h1>Componente 7: Fabian</h1>
+      <C07matrizOperaciones></C07matrizOperaciones>
 
-      <h1>Componente 2:</h1>
-      <C02contador></C02contador>
-
-      <h1>Componente 3:</h1>
-      <C03dobleestado></C03dobleestado>
-
-      <h1>Componente 4:</h1>
-      <C04variable xVariable="hoy dia es viernes..."></C04variable>
-
-      <h1>Componente 5:</h1>
+      <h1>Componente 5: Fabian</h1>
       <C05operador></C05operador>
 
-      <h1>Componente 6:</h1>
-      <C06matriz></C06matriz>
+      <h1>Componente 3: Fabian</h1>
+      <C03dobleestado></C03dobleestado>
+
+      <h1>Componente 1: Fabian</h1>
+      <C01componente></C01componente>
     </div>
   );
 }
